82a365d23a428507304962247dd6f872c25b38dd
Owen Grant <owen.dalmon.grant@gmail.com>
2020-09-28 19:08:14 -0400

    add module descriptor

---
+++ src/main/java/module-info.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+module owen.funwords {
+    requires javafx.controls;
+    requires javafx.graphics;
+    requires javafx.base;
+    requires javafx.fxml;
+    requires com.jfoenix;
+    requires java.logging;
+    
+    opens owen.funwords;
+    opens owen.funwords.controller;
+}
